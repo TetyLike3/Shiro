468f709a504219716032a26c48b28b55f8b887e1
TestyLike3 <95130493+TetyLike3@users.noreply.github.com>
2024-06-12 22:13:40 +0100

    Progression system work

---
+++ src/ReplicatedStorage/Modules/ProgressionModule/init.lua
@@ -0,0 +1,14 @@
+local ProgressionModule = {}
+
+
+ProgressionModule.Races = {
+    Arrancar = "Arrancar",
+    Quincy = "Quincy",
+    Shinigami = "Shinigami",
+}
+
+type PlayerProgData = {
+    Race: string
+}
+
+return ProgressionModule
